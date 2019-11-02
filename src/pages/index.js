0df27c1955d16e8a3910088851a7be73c4f842b2
Aleksefo <aleksefo@gmail.com>
2019-11-02 12:41:19 +0200

    Move Writing to proper position

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import Footer from '../components/Footer'
 const IndexPage = () => (
   <Layout>
     <Header />
-    <Writing />
     <Landing />
     <About />
+    <Writing />
     <Projects />
     <Footer />
   </Layout>
